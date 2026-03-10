c41ecb0e0eb69ca70d898310584ff9bb049ddd98
Flavien <guiboudf@gmail.com>
2026-03-10 10:53:19 +0100

    detection et affichage de quelle case est clicker

--- main.lua
+++ main.lua
@@ -6,10 +6,21 @@ function love.load()
     Tiles.sizeY = Tiles.sizeX
     Tiles.color = {{0, 0, 0},{255, 255, 255}}
     Tiles.color.bg = {255, 255, 255, 0.1}
+
+    Cursor = {}
+    Cursor.posX = 0
+    Cursor.posY = 0
+    Cursor.tileX = 0
+    Cursor.tileY = 0
 end
 
 function love.update(dt)
-
+    if love.mouse.isDown(1) then
+        Cursor.posX = love.mouse.getX()
+        Cursor.posY = love.mouse.getY()
+        Cursor.tileX = math.floor(Cursor.posX / 50)
+        Cursor.tileY = math.floor(Cursor.posY / 50)
+    end
 end
 
 function love.draw()
@@ -29,5 +40,6 @@ function love.draw()
             love.graphics.rectangle("fill", Tiles.posX + i * Tiles.sizeX, Tiles.posY + j * Tiles.sizeY, Tiles.sizeX, Tiles.sizeY)
         end
     end
-    love.graphics.print("Curseur position X : " .. love.mouse.getX() .. ", position Y : " .. love.mouse.getY(), 10, 10)
+    love.graphics.print("Curseur position X : " .. Cursor.posX .. ", position Y : " .. Cursor.posY, 10, 10)
+    love.graphics.print("Indice case X : " .. Cursor.tileX .. ", case Y : " .. Cursor.tileY, 10, 25)
 end
