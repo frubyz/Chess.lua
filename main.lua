1dc12526189f80f9d23087f63bb0f25121be8dbf
Flavien <guiboud.flavien@gmail.com>
2026-03-10 01:28:08 +0100

    Netoyage du code

--- main.lua
+++ main.lua
@@ -1,36 +1,32 @@
-local case = {}
-
-case.posX = 500
-case.posY = 500
-case.sizeX = 50
-case.sizeY = 50
-case.color = {{255, 0, 0, 100},{0, 0, 255, 100},{255, 0, 0, 100},{0, 0, 255, 100},{255, 0, 0, 100},{0, 0, 255, 100},{255, 0, 0, 100},{0, 0, 255, 100}}
-
 function love.load()
-
+    Tiles = {}
+    Tiles.posX = 500
+    Tiles.posY = 500
+    Tiles.sizeX = 50
+    Tiles.sizeY = 50
+    Tiles.color = {{0, 0, 0},{255, 255, 255}}
+    Tiles.color.bg = {255, 255, 255, 0.1}
 end
 
 function love.update(dt)
-    if love.window.isVisible == false then
-        love.window.close()
-    end
+
 end
 
 function love.draw()
     for i = 10, 1, -1 do
         for j = 10, 1, -1 do
-            love.graphics.setColor(255, 255, 255, 0.1)
-            love.graphics.rectangle("fill", case.posX-50*i, case.posY-50*j, case.sizeX, case.sizeY)
+            love.graphics.setColor(Tiles.color.bg)
+            love.graphics.rectangle("fill", Tiles.posX-Tiles.sizeX * i, Tiles.posY-Tiles.sizeY * j, Tiles.sizeX, Tiles.sizeY)
         end
     end
     for i = 8, 1, -1 do
         for j = 8, 1, -1 do
             if (i + j) % 2 == 1 then
-                love.graphics.setColor(0, 0, 0)
+                love.graphics.setColor(Tiles.color[1])
             else
-                love.graphics.setColor(255, 255, 255)
+                love.graphics.setColor(Tiles.color[2])
             end
-            love.graphics.rectangle("fill", case.posX-50-50*i, case.posY-50-50*j, case.sizeX, case.sizeY)
+            love.graphics.rectangle("fill", Tiles.posX-Tiles.sizeX - Tiles.sizeX * i, Tiles.posY - Tiles.sizeY - Tiles.sizeY * j, Tiles.sizeX, Tiles.sizeY)
         end
     end
 end
