c2a20777ba61cc4697d05598862005e3808828a9
Flavien <guiboud.flavien@gmail.com>
2026-03-11 00:04:33 +0100

    Possibiliter de deplacer la dame blanche dans toutes les cases de l'echiquier

--- main.lua
+++ main.lua
@@ -8,17 +8,23 @@ function love.load()
     Tiles.color.bg = {255, 255, 255, 0.1}
 
     Cursor = {}
-    Cursor.posX = 0
-    Cursor.posY = 0
-    Cursor.tileX = 0
-    Cursor.tileY = 0
+    Cursor.pos = 0
+    Cursor.tile = 0
+    Cursor.selected = false
+    Cursor.presssed = false
+
+    Cursor2 = {}
+    Cursor2.pos = 0
+    Cursor2.tile = 0
 
     Chessman = {}
     Chessman[1] = {}
     Chessman[2]= {}
     Chessman[1].texture = {}
     Chessman[2].texture = {}
+    Chessman.selected = {2, 2}
 
+    Blbl = {}
 
     Chessman[1].texture.p = love.graphics.newImage("Textures/Chessman/bp.png")
     Chessman[1].texture.r = love.graphics.newImage("Textures/Chessman/br.png")
@@ -42,11 +48,34 @@ function love.load()
 end
 
 function love.update(dt)
+    Chessman.selected = {2, 2}
+    
+    if love.mouse.isDown(1) and Cursor.selected == true and Cursor.presssed == false and Chessman.selected ~= nil then
+        Cursor2.pos = {love.mouse.getX(), love.mouse.getY()}
+        Cursor2.tile = {math.floor(Cursor2.pos[1] / Tiles.sizeX), math.floor(Cursor2.pos[2] / Tiles.sizeY)}
+        if Cursor.tile[1] == Cursor2.tile[1] and Cursor.tile[2] == Cursor2.tile[2] then
+            Chessman[Chessman.selected[1]].coords[Chessman.selected[2]][2] = Cursor.tile[1]
+            Chessman[Chessman.selected[1]].coords[Chessman.selected[2]][3] = Cursor.tile[2]
+            Cursor.selected = false
+        else
+            Cursor.pos = {love.mouse.getX(), love.mouse.getY()}
+            Cursor.tile = {math.floor(Cursor.pos[1] / Tiles.sizeX), math.floor(Cursor.pos[2] / Tiles.sizeY)}
+        end
+        
+    elseif love.mouse.isDown(1) and Cursor.selected == false and Cursor.presssed == false then
+        Cursor.pos = {love.mouse.getX(), love.mouse.getY()}
+        Cursor.tile = {math.floor(Cursor.pos[1] / Tiles.sizeX), math.floor(Cursor.pos[2] / Tiles.sizeY)}
+        Cursor.selected = true
+    end
+
+    -- if love.mouse.isDown(2) then
+    --     Chessman.selected = ChessmanSelect(Cursor.tile)
+    -- end
+
     if love.mouse.isDown(1) then
-        Cursor.posX = love.mouse.getX()
-        Cursor.posY = love.mouse.getY()
-        Cursor.tileX = math.floor(Cursor.posX / Tiles.sizeX)
-        Cursor.tileY = math.floor(Cursor.posY / Tiles.sizeY)
+        Cursor.presssed = true
+    else
+        Cursor.presssed = false
     end
 end
 
@@ -84,3 +113,14 @@ function love.draw()
         end
     end
 end
+
+-- function ChessmanSelect(pos)
+--     for i = 1, 2, 1 do
+--         for j = 1, 16, 1 do
+--             if Chessman[i].coords[j][2] == pos[1] and Chessman[i].coords[j][3] == pos[2] then
+--                 return {Chessman[i].coords[j][2], Chessman[i].coords[j][3]}
+--             end
+--         end
+--     end
+--     return nil
+-- end
